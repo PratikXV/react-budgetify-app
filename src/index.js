4d75539e31220b9b8c3ebc504305737e7c2742cc
PratikXV <pratik316.sp@gmail.com>
2020-05-23 15:56:24 +0530

    Removed Dummy Code

--- src/index.js
+++ src/index.js
@@ -3,22 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {Provider} from 'react-redux';
 import App from './App';
-import {addExpense} from './actions/expenses';
-import getVisibleExpenses from './selectors/expenses';
 import configureStore from './store/configureStore';
 import * as serviceWorker from './serviceWorker';
 
-
 const store = configureStore();
         console.log(store.getState());
-
-store.dispatch(addExpense({description: 'Rent', amount: 4500, createdAt: 1000}));
-store.dispatch(addExpense({description: 'Water Bill', amount: 1200, createdAt: 2000}));
-store.dispatch(addExpense({description: 'EMI', amount: 1000, createdAt: 3000}));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 const app = (
   <Provider store={store}>
